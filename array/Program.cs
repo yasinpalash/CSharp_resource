a6ce6fb570f2f0bdbace6e05f2693fd8ba173259
yasin <yasinmiapolash@gmail.com>
2025-06-25 00:24:50 +0600

    update

---
+++ array/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+
+class ArrayProgram
+{
+    public static void Main()
+    {
+        int[] a = new int[5];
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 3;
+
+        Console.WriteLine(a[0]); // Output: 1
+        Console.WriteLine(a[1]); // Output: 2
+
+        int[] b = new int[5];
+        for (int i = 0; i < 10; i++)
+        {
+            b[i] = i * 10;
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(b[i]); // Output: 0, 10, 20, ..., 90      
+        }
+
+
+        //2D array
+
+        int[][] c = new int[3][];
+        
+        //array of arrays
+
+        for (int i = 0; i < c.Length; i++)
+        {
+            c[i] = new int[5];
+            for (int j = 0; j < 5; j++)
+            {
+                c[i] = new int[3];
+            }
+        }
+    }
+    
+
+
+
+    
+
+
+
+
+
+
+}
+
+
--- loop/Program.cs
+++ loop/Program.cs
@@ -1,2 +1,21 @@
 ﻿
-Console.WriteLine("Hello, Yasin!");
+using System;
+
+
+class Program
+{
+    public static void Main()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        int a = 1;
+        while(a <= 10)
+        {
+            Console.WriteLine(a);
+            a++;
+        }
+    }
+}
